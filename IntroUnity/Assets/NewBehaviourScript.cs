040ef62dc1301a9bd15f437cc385dd496a1cb592
carlosgijon <carlosgijonfernandez@gmail.com>
2018-06-13 23:40:59 +0200

    subida 13/06

---
+++ IntroUnity/Assets/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+    int miCasa = 1;
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Hola en consola");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log("Hola en consola");
+	}
+}
